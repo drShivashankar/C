9a96bd60c04a1fd902a4cc914c8fd3fe3adcc524
drShivashankar <59251618+drShivashankar@users.noreply.github.com>
2019-12-26 16:15:27 +0530

    Output: Datatypes.c
    Shivas-MacBook-Pro:C shivashankardr$ gcc 1.c 
    Shivas-MacBook-Pro:C shivashankardr$ ./a.out 
    x1= and char size is 1 byte(s) 
    x2=127 and signed char size is 1 byte(s) 
    x3=? and unsigned char size is 1 byte(s) 
    x4=0 and short size is 2 byte(s) 
    x5=0 and short int size is 2 byte(s) 
    x6=0 and signed short size is 2 byte(s) 
    x7=0 and signed short int size is 2 byte(s) 
    x8=0 and unsigned short size is 2 byte(s) 
    x9=0 and unsigned short int size is 2 byte(s) 
    x10=0  and int size is 4 byte(s) 
    x11=0  and signed size is 4 byte(s) 
    x12=0  and signed int size is 4 byte(s) 
    x13=0 and unsigned size is 4 byte(s) 
    x14=0 and unsigned int size is 4 byte(s) 
    x15=0 and long size is 8 byte(s) 
    x16=0 and long int size is 8 byte(s) 
    x17=0 and signed long size is 8 byte(s) 
    x18=0 and signed long int size is 8 byte(s) 
    x19=0 and unsigned long size is 8 byte(s) 
    x20=0 and unsigned long int size is 8 byte(s) 
    x21=0 and long long size is 8 byte(s) 
    x22=0 and long long int size is 8 byte(s) 
    x23=0 and signed long long size is 8 byte(s) 
    x24=0 and signed long long int size is 8 byte(s) 
    x25=0 and unsigned long long size is 8 byte(s) 
    x26=0 and unsigned long long int size is 8 byte(s) 
    x27=0.000000 and float size is 4 byte(s) 
    x28=0 and float size is 4 byte(s) 
    x29=0.000000e+00 and float size is 4 byte(s) 
    x30=0x0p+0 and float size is 4 byte(s) 
    x31=0.000000  and double size is 8 byte(s) 
    x32=0 and double size is 8 byte(s) 
    x33=0.000000e+00 and double size is 8 byte(s) 
    x34=0x0p+0 and double size is 8 byte(s) 
    x35=0.000000 and long double size is 16 byte(s) 
    x36=0 and long double size is 16 byte(s) 
    x37=0.000000e+00 and long double size is 16 byte(s) 
    x38=0x0p+0 and long double size is 16 byte(s) 
    Shivas-MacBook-Pro:C shivashankardr$

---
+++ Datatypes.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+int main(){
+	char x1=0; 
+	printf("x1=%c and char size is %lu byte(s) \n",x1, sizeof(x1));
+
+	signed char x2=127; 
+	printf("x2=%i and signed char size is %lu byte(s) \n",x2, sizeof(x2));
+
+	unsigned char x3=128; 
+	printf("x3=%c and unsigned char size is %lu byte(s) \n",x3, sizeof(x3));
+
+	short x4=0; 
+	printf("x4=%hi and short size is %lu byte(s) \n",x4, sizeof(x4));
+
+	short int x5=0; 
+	printf("x5=%hi and short int size is %lu byte(s) \n",x5, sizeof(x5));
+
+	signed short x6=0; 
+	printf("x6=%hi and signed short size is %lu byte(s) \n",x6, sizeof(x6));
+
+	signed short int x7=0; 
+	printf("x7=%hi and signed short int size is %lu byte(s) \n",x7, sizeof(x7));
+
+	unsigned short x8=0; 
+	printf("x8=%hu and unsigned short size is %lu byte(s) \n",x8, sizeof(x8));
+
+	unsigned short int x9=0; 
+	printf("x9=%hu and unsigned short int size is %lu byte(s) \n",x9, sizeof(x9));
+
+	int x10=0; 
+	printf("x10=%i  and int size is %lu byte(s) \n",x10, sizeof(x10));
+
+	signed x11=0; 
+	printf("x11=%i  and signed size is %lu byte(s) \n",x11, sizeof(x11));
+
+	signed int x12=0; 
+	printf("x12=%i  and signed int size is %lu byte(s) \n",x12, sizeof(x12));
+
+	unsigned x13=0; 
+	printf("x13=%u and unsigned size is %lu byte(s) \n",x13, sizeof(x13));
+
+	unsigned int x14=0; 
+	printf("x14=%u and unsigned int size is %lu byte(s) \n",x14, sizeof(x14));
+
+	long x15=0; 
+	printf("x15=%li and long size is %lu byte(s) \n",x15, sizeof(x15));
+
+	long int x16=0; 
+	printf("x16=%li and long int size is %lu byte(s) \n",x16, sizeof(x16));
+
+	signed long x17=0; 
+	printf("x17=%li and signed long size is %lu byte(s) \n",x17, sizeof(x17));
+
+	signed long int x18=0; 
+	printf("x18=%li and signed long int size is %lu byte(s) \n",x18, sizeof(x18));
+
+	unsigned long x19=0; 
+	printf("x19=%lu and unsigned long size is %lu byte(s) \n",x19, sizeof(x19));
+
+	unsigned long int x20=0; 
+	printf("x20=%lu and unsigned long int size is %lu byte(s) \n",x20, sizeof(x20));
+
+	long long x21=0; 
+	printf("x21=%lli and long long size is %lu byte(s) \n",x21, sizeof(x21));
+
+	long long int x22=0; 
+	printf("x22=%lli and long long int size is %lu byte(s) \n",x22, sizeof(x22));
+
+	signed long long x23=0; 
+	printf("x23=%lli and signed long long size is %lu byte(s) \n",x23, sizeof(x23));
+
+	signed long long int x24=0; 
+	printf("x24=%lli and signed long long int size is %lu byte(s) \n",x24, sizeof(x24));
+
+	unsigned long long x25=0; 
+	printf("x25=%llu and unsigned long long size is %lu byte(s) \n",x25, sizeof(x25));
+
+	unsigned long long int x26=0; 
+	printf("x26=%llu and unsigned long long int size is %lu byte(s) \n",x26, sizeof(x26));
+
+	float x27=0; 
+	printf("x27=%f and float size is %lu byte(s) \n",x27, sizeof(x27));
+
+	float x28=0; 
+	printf("x28=%g and float size is %lu byte(s) \n",x28, sizeof(x28));
+
+	float x29=0; 
+	printf("x29=%e and float size is %lu byte(s) \n",x29, sizeof(x29));
+
+	float x30=0; 
+	printf("x30=%a and float size is %lu byte(s) \n",x30, sizeof(x30));
+
+	double x31=0; 
+	printf("x31=%lf  and double size is %lu byte(s) \n",x31, sizeof(x31));
+
+	double x32=0; 
+	printf("x32=%lg and double size is %lu byte(s) \n",x32, sizeof(x32));
+
+	double x33=0; 
+	printf("x33=%le and double size is %lu byte(s) \n",x33, sizeof(x33));
+
+	double x34=0; 
+	printf("x34=%la and double size is %lu byte(s) \n",x34, sizeof(x34));
+
+	long double x35=0; 
+	printf("x35=%Lf and long double size is %lu byte(s) \n",x35, sizeof(x35));
+
+	long double x36=0; 
+	printf("x36=%Lg and long double size is %lu byte(s) \n",x36, sizeof(x36));
+
+	long double x37=0; 
+	printf("x37=%Le and long double size is %lu byte(s) \n",x37, sizeof(x37));
+
+	long double x38=0; 
+	printf("x38=%La and long double size is %lu byte(s) \n",x38, sizeof(x38));
+}
